269ff6559b6e0e61ca4013094e98370a1d13eab7
Shaun Thornburgh <personal@shaunthornburgh.com>
2017-01-17 22:16:17 +0000

    Implement image upload

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170116235648) do
+ActiveRecord::Schema.define(version: 20170117213947) do
+
+  create_table "images", force: :cascade do |t|
+    t.string   "name"
+    t.string   "picture"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "images", ["user_id"], name: "index_images_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
